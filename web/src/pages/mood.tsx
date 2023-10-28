b55f3314971c312b890c5b07047c329790b1e36e
imedra <marcbernardino2005@gmail.com>
2023-10-28 04:29:04 -0700

    added template before backend integ

--- web/src/pages/mood.tsx
+++ web/src/pages/mood.tsx
@@ -8,40 +8,9 @@ import { api } from '../../convex/_generated/api.js';
 const emojiBank = '😀,😁,🥹,😅,😂,🥲,😊,🙂,😌,🥰,😋,😝,🤪,😎,🤩,🥳,😒,😞,😔,😟,😖,😩,🥺,😢,😭,😤,😡,🤬,🤯,😳,😱,😰,😓,🤗,🤭,😐,😬,😯,😧,😮,😴,😪,😮,‍🤐,🤧,😷,🤒,🤕,🤠'
 
 export default function Mood() {
-    const CLIENT_ID = "1526c36afb8b45ac8e684bb8729215b6";
-    const REDIRECT_URI = "http://localhost:3000/mood";
     const emojiBank = '😀,😁,🥹,😅,😂,🥲,😊,🙂,😌,🥰,😋,😝,🤪,😎,🤩,🥳,😒,😞,😔,😟,😖,😩,🥺,😢,😭,😤,😡,🤬,🤯,😳,😱,😰,😓,🤗,🤭,😐,😬,😯,😧,😮,😴,😪,😮,‍🤐,🤧,😷,🤒,🤕,🤠';
-    let [mood, setMood] = useState(emojiBank.split(',')[0]);
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const code = urlParams.get('code');
-        console.log('Code:', code);
-        if (code) {
-            axios.post('https://fiery-panda-598.convex.cloud/getAuth', { code }).then((response) => {
-                const accessToken = response.data.accessToken;
-                axios.get('https://api.spotify.com/v1/me/playlists', {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                }).then((response) => {
-                    const playlists = response.data.items;
-                    playlists.forEach((playlist: any) => {
-                        axios.get('https://api.spotify.com/v1/me/tracks', {
-                            headers: {
-                                'Authorization': `Bearer ${accessToken}`
-                            }
-                        }).then((response) => {
-                            const tracks = response.data.items;
-                            tracks.forEach((track: any) => {
-                                console.log(track.track.name);
-                            });
-                        });
-                    });
-                });
-            });
-        }
-    }, []);
+    let [mood, setMood] = useState(emojiBank.split(',')[0])
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
