6fd6b186bd4389762eb4a409d155b55cbb552c41
mayuridev <dylaniskandar23@gmail.com>
2023-10-27 20:47:46 -0700

    spotify auth

--- web/src/pages/auth.tsx
+++ web/src/pages/auth.tsx
@@ -1,13 +1,46 @@
-import Link from 'next/link'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export default function Auth() {
+    const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+    const CLIENT_ID = "your_spotify_app_client_id"
+    const REDIRECT_URI = "your_app_redirect_uri"
+    const SCOPES = "user-library-read"
+
+    const handleConnectToSpotify = () => {
+        window.location.href = `${SPOTIFY_AUTH_URL}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}&response_type=code`
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search)
+        const code = urlParams.get('code')
+
+        if (code) {
+            axios.post('https://accounts.spotify.com/api/token', {
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: REDIRECT_URI,
+                client_id: CLIENT_ID,
+                client_secret: 'your_spotify_app_client_secret'
+            }).then((response: any) => {
+                const accessToken = response.data.access_token
+                axios.get('https://api.spotify.com/v1/me/tracks', {
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                }).then((response: any) => {
+                    console.log(response.data)
+                })
+            })
+        }
+    }, [])
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="flex flex-col md:flex-row pt-2 md:space-x-2">
-                <Link className="btn btn-success" href="/auth">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mr-2" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z"/> </svg>
+                <button onClick={handleConnectToSpotify} className="btn btn-success">
                     Connect to Spotify
-                </Link>          
+                </button>
             </div>
         </div>
     )
