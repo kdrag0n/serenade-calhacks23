f9498208603dd1ecc851617c6ec922adf1e63baa
imedra <marcbernardino2005@gmail.com>
2023-10-28 15:50:23 -0700

    added spotify auth via clerk

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Productionize .env
+const convex = new ConvexReactClient("https://fiery-panda-598.convex.cloud");
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider publishableKey="pk_test_aGVscGZ1bC1nb2JibGVyLTgxLmNsZXJrLmFjY291bnRzLmRldiQ">
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Component {...pageProps} />
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
