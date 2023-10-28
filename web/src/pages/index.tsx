56fe8e9673b16980c78bac6afcf4297a8c978a2d
imedra <marcbernardino2005@gmail.com>
2023-10-28 15:58:40 -0700

    more auth

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState } from 'react'
-import Auth from './auth'
+import { useConvexAuth } from "convex/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
   return (
     <main className="bg-gradient-to-b from-[#11998e] to-[#237A57] min-h-screen">
+      <div>
+        {isAuthenticated ? "Logged in" : "Logged out or still loading"}
+      </div>
     </main>
   )
 }
