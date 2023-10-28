58e5b9f42c41c935626e48ef1f9df546485d0fa9
imedra <marcbernardino2005@gmail.com>
2023-10-28 16:13:19 -0700

    automated login

--- web/src/pages/auth.tsx
+++ web/src/pages/auth.tsx
@@ -1,18 +1,14 @@
-import { SignInButton } from "@clerk/clerk-react";
+import { useRouter } from 'next/router';
+import { RedirectToSignIn } from "@clerk/clerk-react";
 import { useConvexAuth } from "convex/react";
 
 export default function Auth() {
     const { isLoading, isAuthenticated } = useConvexAuth();
+    const router = useRouter();
+
         return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="flex flex-col md:flex-row pt-2 md:space-x-2">
-                <button className="btn btn-success">
-                    <SignInButton/>
-                </button>
-            </div>
-            <div>
-                {isAuthenticated ? "Logged in" : "Logged out or still loading"}
-            </div>
+        <div className="flex min-h-screen bg-gradient-to-b from-[#11998e] to-[#237A57] flex-col items-center justify-between p-24">
+            {isAuthenticated ? router.push('/') : <RedirectToSignIn/>}
         </div>
     )
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,18 +1,31 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useState } from 'react'
+import { useEffect } from "react";
+import { useRouter } from 'next/router';
 import { useConvexAuth } from "convex/react";
+import { SignOutButton } from '@clerk/clerk-react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const { isLoading, isAuthenticated } = useConvexAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+      // Check the user's authentication status
+      if (isAuthenticated == false && isLoading == false) {
+        // If the user is not authenticated, redirect to the login page
+        router.push('/auth');
+      }
+    }, [isAuthenticated, isLoading]);
 
   return (
-    <main className="bg-gradient-to-b from-[#11998e] to-[#237A57] min-h-screen">
+    <main className="flex min-h-screen bg-gradient-to-b from-[#11998e] to-[#237A57] flex-col items-center space-y-10 p-24">
       <div>
         {isAuthenticated ? "Logged in" : "Logged out or still loading"}
       </div>
+      <button className="btn btn-success">
+        <SignOutButton/>
+      </button> 
     </main>
   )
 }
