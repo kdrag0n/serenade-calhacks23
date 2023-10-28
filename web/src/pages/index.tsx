124f79abd6a879f08108085e169e61a10ce3bbf2
imedra <marcbernardino2005@gmail.com>
2023-10-27 20:29:16 -0700

    transferred mood, auth to individual components

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,26 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useState } from 'react'
+import Auth from './auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const emojiBank = '😀,😃,😁,🥹,😅,😂,🥲,😊,🙂,😌,🥰,😙,😋,😝,🤪,🧐,🤓,😎,🤩,🥳,😒,😞,😔,😟,☹,️,😖,😫,😩,🥺,😢,😭,😤,😠,😡,🤬,🤯,😳,🥵,😱,😰,😥,😓,🤗,🤔,🤭,🫡,🤥,😐,😬,😯,😦,😧,😮,😲,😴,😪,😮,‍,😵,‍,💫,🤐,🤧,😷,🤒,🤕,🤠'
-
 export default function Home() {
-  let [mood, setMood] = useState(emojiBank.split(',')[0])
-
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      {/* mood picker */}
-      <div className='flex flex-row items-center justify-center flex-wrap'>
-        {emojiBank.split(',').map((emoji, i) => (
-          <button key={i} onClick={() => setMood(emoji)} className='text-6xl'>
-            {emoji}
-          </button>
-        ))}
-      </div>
+    <main>
     </main>
   )
 }
---
+++ web/src/pages/mood.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
+const emojiBank = '😀,😃,😄,😁,😆,🥹,😅,😂,🤣,🥲,😊,😇,🙂,🙃,😉,😌,😍,🥰,😘,😗,😙,😚,😋,😛,😝,😜,🤪,🤨,🧐,🤓,😎,🥸,🤩,🥳,😏,😒,😞,😔,😟,😕,🙁,☹,️,😣,😖,😫,😩,🥺,😢,😭,😤,😠,😡,🤬,🤯,😳,🥵,🥶,😶,‍,🌫,️,😱,😨,😰,😥,😓,🤗,🤔,🫣,🤭,🫢,🫡,🤫,🫠,🤥,😶,🫥,😐,🫤,😑,🫨,😬,🙄,😯,😦,😧,😮,😲,🥱,😴,🤤,😪,😮,‍,💨,😵,😵,‍,💫,🤐,🥴,🤢,🤮,🤧,😷,🤒,🤕,🤑,🤠'
+
+export default function Mood() {
+    let [mood, setMood] = useState(emojiBank.split(',')[0])
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        {/* mood picker */}
+        <div className='flex flex-row items-center justify-center flex-wrap'>
+            {emojiBank.split(',').map((emoji, i) => (
+            <button key={i} onClick={() => setMood(emoji)} className='text-6xl'>
+                {emoji}
+            </button>
+            ))}
+        </div> 
+      </div>   
+    )
+}
