c984030cab05469c45e2f14dd742e0456a41189b
imedra <marcbernardino2005@gmail.com>
2023-10-28 17:24:34 -0700

    spotify grab working

---
+++ web/src/pages/api/getTracks.ts
@@ -0,0 +1,34 @@
+import { Clerk } from '@clerk/clerk-sdk-node';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  // Initialize Clerk
+  const clerk = Clerk({ secretKey: 'sk_test_tuvlhnoT55OF5X28tq0sGR1G3PRLCkJU3eInTjivtP' });
+  const userId = await req.query.userId as string;
+
+  try {
+    // Get the user's OAuth token
+    const OAuthAccessToken = await clerk.users.getUserOauthAccessToken(userId, 'oauth_spotify');
+    const token = OAuthAccessToken[0].token;
+    // Fetch data from the external resource
+    const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=1', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200) {
+      const data = await response.json();
+      res.status(200).json(data);
+    } else {
+      res.status(response.status).json({ message: 'Error fetching data from the external resource' });
+    }
+  } catch (error) {
+    console.error('Internal server error: ', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,31 +1,49 @@
-import { Inter } from 'next/font/google'
-import { useEffect } from "react";
+import { Inter } from 'next/font/google';
+import { use, useEffect, useState } from "react";
 import { useRouter } from 'next/router';
 import { useConvexAuth } from "convex/react";
 import { SignOutButton } from '@clerk/clerk-react';
+import { useAuth } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const { isLoading, isAuthenticated } = useConvexAuth();
+  const { userId, isLoaded } = useAuth();
   const router = useRouter();
+  const [token, setToken] = useState<string | null>(null);
+  
+  const fetchDataFromExternalResource = async () => {
+    try {
+      const response = await fetch(`/api/getTracks?userId=${userId}`);
+      const data = await response.json();
+      return data; 
+    } catch (error) {
+      console.error("Error fetching data from external resource:", error);
+      return null;
+    }
+  };
 
   useEffect(() => {
-      // Check the user's authentication status
-      if (isAuthenticated == false && isLoading == false) {
-        // If the user is not authenticated, redirect to the login page
-        router.push('/auth');
-      }
-    }, [isAuthenticated, isLoading]);
+    if (isAuthenticated === false && isLoading === false) {
+      router.push('/auth');
+    } if (isAuthenticated === true && userId) {
+      fetchDataFromExternalResource();
+    }
+  }, [isAuthenticated, isLoading, isLoaded, router]);
 
   return (
     <main className="flex min-h-screen bg-gradient-to-b from-[#11998e] to-[#237A57] flex-col items-center space-y-10 p-24">
       <div>
-        {isAuthenticated ? "Logged in" : "Logged out or still loading"}
+        {isAuthenticated ? (
+          <p>
+            Logged in {token}
+          </p>
+        ) : "Logged out or still loading"}
       </div>
       <button className="btn btn-success">
-        <SignOutButton/>
-      </button> 
+        <SignOutButton />
+      </button>
     </main>
-  )
+  );
 }
